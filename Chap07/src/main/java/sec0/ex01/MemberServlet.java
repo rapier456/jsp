b10214603c3ba0080fc9f54f763dedd3f1f78d1b
rapier456 <127849500+rapier456@users.noreply.github.com>
2023-05-04 16:03:22 +0900

    Add files via upload

---
+++ Chap07/src/main/java/sec0/ex01/MemberServlet.java
@@ -0,0 +1,130 @@
+package sec0.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MemberServlet
+ */
+@WebServlet("/Member4")
+public class MemberServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+	      PrintWriter out=response.getWriter();	
+	      MemberDAO dao=new MemberDAO();
+	      
+	      String command = request.getParameter("command");
+	      if(command !=null && command.equals("addMember")) {
+	    	  String _id = request.getParameter("id");
+	    	  String _pwd = request.getParameter("pwd");
+	    	  String _name = request.getParameter("name");
+	    	  String _email = request.getParameter("email");
+	    	  MemberVO vo = new MemberVO();
+	    	  vo.setId(_id);
+	    	  vo.setPwd(_pwd);
+	    	  vo.setName(_name);
+	    	  vo.setEmail(_email);
+	    	  dao.addMember(vo);
+	      }
+	      List<MemberVO> list=dao.listMembers();
+		
+	      out.print("<html><body>");
+	      out.print("<table  border=1><tr align='center' bgcolor='lightgreen'>");
+	      out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일</td></tr>");
+	     
+	     for (int i=0; i<list.size();i++){
+			MemberVO memberVO=(MemberVO) list.get(i);
+			String id=memberVO.getId();
+			String pwd = memberVO.getPwd();
+			String name=memberVO.getName();
+			String email=memberVO.getEmail();
+			Date joinDate = memberVO.getJoinDate();
+			out.print("<tr><td>"+id+"</td><td>"+
+				                pwd+"</td><td>"+
+				                name+"</td><td>"+
+				                email+"</td><td>"+
+				                joinDate+"</td></tr>");		
+	      }
+	      out.print("</table></body></html>");
+	      out.print("<a href='/Chap07/memberForm.html'>새 회원 등록하기</a>");
+	
+	}
+
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doDelete(req, resp);
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+
+	@Override
+	protected void doHead(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doHead(arg0, arg1);
+	}
+
+	@Override
+	protected void doOptions(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doOptions(arg0, arg1);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPut(req, resp);
+	}
+
+	@Override
+	protected void doTrace(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doTrace(arg0, arg1);
+	}
+
+	@Override
+	protected long getLastModified(HttpServletRequest req) {
+		// TODO Auto-generated method stub
+		return super.getLastModified(req);
+	}
+
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(arg0, arg1);
+	}
+
+	@Override
+	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(arg0, arg1);
+	}
+
+
+}
